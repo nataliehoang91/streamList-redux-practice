b5d27a6fefdc2854035f16c26149499434fadd8b
Nat <natalie.hoang91@gmail.com>
2019-06-28 11:07:36 +0700

    first commit

---
+++ src/components/streams/StreamDelete.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const StreamDelete = () => {
+  return(
+  <div>Stream Create</div>)
+};
+
+export default StreamDelete;
---
+++ src/components/streams/StreamList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StreamList = () => {
+  return <div><h1>SreamList</h1></div>;
+};
+
+export default StreamList;
